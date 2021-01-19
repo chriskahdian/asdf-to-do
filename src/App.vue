a3564ca97034271621f28e2d3e8ea52731664999
Chris Kahdian <chriskahdian@gmail.com>
2021-01-19 09:48:42 -0800

    202101190947

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,62 @@
 <template>
-  <div>
-    <input
-      v-model="currentTodo"
-      @keydown.enter="addTodo()"
-      placeholder="Add a todo"
-    />
-    <ul class="todos">
-      <li
-        v-for="todo in todos"
-        :key="todo.id"
-        :class="{
-          todo: true,
-          editing: editedTodo === todo,
-          completed: todo.completed
-        }"
-        @dblclick="editTodo(todo)"
+  <div class="bigDiv">
+    <md-field>
+      <md-input
+        v-model="currentTodo"
+        @keydown.enter="addTodo()"
+        placeholder="Add a todo; double click to edit; drag to rearrange"
+        class="addATodo"
       >
-        <input
-          class="check"
-          type="checkbox"
-          v-model="todo.completed"
-        />
-        {{ todo.label }}
-        <button @click="removeTodo(todo)">x</button>
-          <input
-            class="edit"
-            v-model="todo.label"
-            @blur="cancelEdit(todo)"
-            @keyup.esc="cancelEdit(todo)"
-            @keyup.enter="doneEdit(todo)"
-          />
-      </li>
-    </ul>
+      </md-input>
+    </md-field>
+    <draggable
+      class="dragContainer"
+      v-model="todos"
+      @start="drag = true"
+      @end="drag = false"
+    >
+        <md-card
+          md-with-hover
+          v-for="todo in todos"
+          :key="todo.id"
+          :class="{
+              todo: true,
+              editing: editedTodo === todo,
+              completed: todo.completed
+            }"
+        >
+          <md-card-content>
+            <div class="regular">
+              <input
+                class="check"
+                type="checkbox"
+                v-model="todo.completed"
+              />
+              <span @dblclick="editTodo(todo)">
+                {{ todo.label }}
+              </span>
+              <button class="remove" @click="removeTodo(todo)" >x</button>
+            </div>
+            <input
+              type="text"
+              class="edit"
+              v-model="todo.label"
+              @blur="cancelEdit(todo)"
+              @keyup.esc="cancelEdit(todo)"
+              @keyup.enter="doneEdit(todo)"
+            />
+          </md-card-content>
+        </md-card>
+    </draggable>
   </div>
 </template>
 
 <script>
+import draggable from "vuedraggable";
 export default {
+  components: {
+    draggable,
+  },
   data() {
     return {
       todos: [],
@@ -75,19 +95,54 @@ export default {
       }
       this.editedTodo = null;
       todo.label = this.originalEditedTodoValue;
-    }
+    },
   },
 };
 </script>
 
 <style>
+  .regular{
+  }
   .edit {
     display: none;
   }
-  li.editing .view {
+  .editing .edit {
+    display: block;
+    margin-left: 20px
+  }
+  .editing .regular{
     display: none;
   }
-  li.editing .edit {
-    display: block;
+  .bigDiv{
+    display: flex;
+    flex-direction: column;
+    min-width: 300px;
+    max-width: 70%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    border: 1px solid;
+    text-align: left;
+    overflow: hidden;
+  }
+  .dragContainer{
+    display: flex;
+    flex-direction: column;
+  }
+  md-card{
+    margin-left: 30px;
+    border: solid;
+  }
+  .addATodo{
+    text-align: center;
+  }
+  .remove{
+    transform: scale(0.5);
+  }
+  .completed {
+    text-decoration: line-through;
+  } 
+  .remove{
+    float: right;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { MdField } from 'vue-material/dist/components';
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 
-Vue.use(MdField);
+import { MdField } from 'vue-material/dist/components'
+
+Vue.use(VueMaterial)
+Vue.use(MdField)
 
 Vue.config.productionTip = false
 
